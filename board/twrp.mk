69ef6a15fa06fb2af182ca277acb9fd7453524df
Nikolas Lim <nicknitewolf@lineageos.org>
2018-06-16 02:25:01 +0800

    wt88047: Additional TWRP flags
    Change-Id: I37aa2b1a123399d44547a4538298d2c2b03992d4

--- board/twrp.mk
+++ board/twrp.mk
@@ -2,4 +2,6 @@ ifeq ($(WITH_TWRP),true)
 TARGET_RECOVERY_DEVICE_DIRS += device/wingtech/wt88047/twrp
 TW_INCLUDE_CRYPTO := true
 TW_THEME := portrait_hdpi
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 endif
